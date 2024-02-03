2bb85f648a4a895f21c4223ffa578f78ae261a31
cpop <prabhsharan971@gmail.com>
2024-02-03 16:12:10 +0530

    analyzing data pts- analyzerNode props

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const input = document.querySelector("input");
 const audioElem = document.querySelector("audio");
+const canvas = document.querySelector("canvas");
+
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 input.addEventListener("change", ()=> {
     const file = input.files[0];
@@ -24,5 +30,25 @@ input.addEventListener("change", ()=> {
     
     // connect to final destination of audio - eg. speakers
     analyser.connect(audioContext.destination);    
+
+    //Fast Fourier Transform algo is used to get freq domain data
+    // fftSize- prop of analyzerNode -window size of samples- 2^5 to ^15
+    analyser.fftSize = 512;
+
+    const bufferDataLength = analyser.frequencyBinCount; //total data pts available, half of fftSize
+
+    const dataArr = new Uint8Array(bufferDataLength); //create unsigned 8 bit unt array, 8 bits= 1 byte
+
+    // setInterval(function(){
+    //     analyser.getByteFrequencyData(dataArr);
+    //     console.log(dataArr);
+    // },2000)
     
+    function createSoundBars(){
+        analyser.getByteFrequencyData(dataArr); // get frew data from samples
+
+        bufferDataArr.forEach(dataValue =>{
+
+        })
+    }
 })
